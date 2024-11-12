9d3b856f3352b79a47de4e7854e3f7e95922882d
Juan <109253323+bautistaJuan@users.noreply.github.com>
2024-11-11 23:00:43 -0300

    Update ServicesCard.tsx

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -4,7 +4,7 @@ const ServicesCard = ({ service, index }: { service: Service, index: number }) =
     return (
         <div key={index} className="border rounded-xl overflow-auto gap-1 sm:flex w-full lg:pr-2 ">
             <img className="w-full lg:w-[35rem] h-[20rem] lg:h-[13rem]" src={service.img} alt={service.title} />
-            <div className="flex flex-col">
+            <div className="flex flex-col p-2">
                 <h2 className="text-[2rem] text-stroke text-transparent ">{service.title}</h2>
                 <p className="flex flex-grow text-justify">
                     {service.description}
