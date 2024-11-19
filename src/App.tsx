1a2f65da703820246b0fe3506f645ced625d48f4
Juan Bautista Vega <jv604824@gmail.com>
2024-11-18 21:53:50 -0300

    Update: Remove StrictMode and add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layaout from './components/Layaout';
 import Home from './pages/Home';
 import Error from './pages/Error';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layaout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layaout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
